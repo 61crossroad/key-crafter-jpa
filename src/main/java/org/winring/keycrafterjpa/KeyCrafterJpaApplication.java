da35d70512f3a94430ff809743c7dadfed8c28af
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-02 00:59:33 +0900

    create key-crafter-jpa rest api boilerplate

---
+++ src/main/java/org/winring/keycrafterjpa/KeyCrafterJpaApplication.java
@@ -0,0 +1,13 @@
+package org.winring.keycrafterjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyCrafterJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyCrafterJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/winring/keycrafterjpa/KeyCrafterJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.winring.keycrafterjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyCrafterJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
